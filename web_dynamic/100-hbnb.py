69b95ec76f50cc47b5dc04adcb7d7db0e236bdf8
Bismark-A <bismarkamuna007@gmail.com>
2023-05-09 00:07:59 +0000

    web_dynamic

--- web_dynamic/100-hbnb.py
+++ web_dynamic/100-hbnb.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+"""starts a Flash Web Application """
 from models import storage
 from models.state import State
 from models.city import City
@@ -19,30 +19,29 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb/', strict_slashes=False)
+@app.route('/100-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)  # Sort states by name attribute
-
+    states = sorted(states, key=lambda k: k.name)
     st_ct = []
+
     for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])  # Sort cities by name attribute
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
 
     amenities = storage.all(Amenity).values()
-    amenities = sorted(amenities, key=lambda k: k.name)  # Sort amenities by name attribute
+    amenities = sorted(amenities, key=lambda k: k.name)
 
     places = storage.all(Place).values()
-    places = sorted(places, key=lambda k: k.name)  # Sort places by name attribute
+    places = sorted(places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html',
+    return render_template('100-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
-                           cache_id=uuid.uuid4())
+                           cache_id=str(uuid.uuid4()))
 
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5001)
-~
+    app.run(host='0.0.0.0', port=5000)
